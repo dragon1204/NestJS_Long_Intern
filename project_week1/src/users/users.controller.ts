5202233dcefeefbe2a3f8dea42b7451224ee03c0
Long <vungoclong20041204@gmail.com>
2025-06-18 17:28:55 +0700

    hoan thanh week 2

--- project_week1/src/users/users.controller.ts
+++ project_week1/src/users/users.controller.ts
@@ -11,4 +11,24 @@ export class UsersController {
         return this.usersService.findAllUsers();
     }
 
+    @Get("find/:id")
+    async findUserById(@Param('id') id: string) {
+        return this.usersService.findUserById(+id);
+    }       
+
+    @Post("create")
+    async createUser(@Body() userData: any) {
+        return this.usersService.createUser(userData);
+    }   
+
+    @Put(":id")
+    async updateUser(@Param('id') id: string, @Body() userData: any) {
+        return this.usersService.updateUser(+id, userData);
+    }       
+
+    @Delete(":id")
+    async deleteUser(@Param('id') id: string) {
+        return this.usersService.deleteUser(+id);
+    }
+
 }
