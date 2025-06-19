b45af9b01e1909af857b571615b602e73a0989a3
Long <vungoclong20041204@gmail.com>
2025-06-19 15:19:27 +0700

    week3

--- Project_IOT/src/app.module.ts
+++ Project_IOT/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { ControlerController } from './controler/controler.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [UsersModule, AuthModule],
+  controllers: [AppController, ControlerController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
---
+++ Project_IOT/src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { sign } from 'crypto';
+import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './auth.jwt.strategy';
+
+
+@Module({
+    imports: [
+        UsersModule, 
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+
+            signOptions: {
+                expiresIn: '60s' // Token expiration time
+            },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
+    exports: [PassportModule, JwtModule]
+})
+export class AuthModule {}
